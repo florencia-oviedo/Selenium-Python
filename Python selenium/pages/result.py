d54ec2076d5a1cc0042acc1bbfe63f4434e82b5d
Florencia Micaela Oviedo <flor.oviedo2010@gmail.com>
2023-09-21 13:03:03 -0300

    find elements

--- Python selenium/pages/result.py
+++ Python selenium/pages/result.py
@@ -2,9 +2,12 @@
 This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
-
+from selenium.webdriver.common.by import By
 
 class DuckDuckGoResultPage:
+
+  RESULT_LINKS = (By.CSS_SELECTOR,'#react-layout > div > div > section.At_VJ9MlrHsSjbfCtz2_.aDtqDaYogch0DyrGTrX6 > ol')
+  SEARCH_INPUTS = (By.ID,'search_form_input')
   
   def __init__(self, browser):
     self.browser = browser
